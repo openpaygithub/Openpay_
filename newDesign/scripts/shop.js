9815974ac67bb025e344f6c2f4cc87833840af32
Oleksandr Zaharchenko <oleksanderz>
2018-11-20 16:35:23 +0200

    search brand subpage

--- newDesign/scripts/shop.js
+++ newDesign/scripts/shop.js
@@ -1,148 +1,40 @@
-var PAGE_SIZE_OFFSET = 21;
-
-function parseParams(str) {
-    if (str.trim() === '') return {};
-
-    return str.split('&').reduce(function (params, param) {
-        var paramSplit = param.split('=').map(function (value) {
-            return decodeURIComponent(value.replace(/\+/g, ' '));
-        });
-        params[paramSplit[0]] = paramSplit[1];
-        return params;
-    }, {});
-}
-
-function modifySearchParams(patch) {
-    if (!history.pushState || !Object.assign) return console.error('Update your browser');
-
-    var currentParams = parseParams(location.search.substring(1));
-    var newParams = Object.assign({}, currentParams, patch);
-    var path = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + $.param(newParams);
-
-    window.history.pushState({ path: path }, '', path);
-}
-
-function populateControls() {
-    var currentParams = parseParams(location.search.substring(1));
-    if (currentParams.CategoryID) $('#category').val(currentParams.CategoryID);
-    if (currentParams.Keyword) $('#query').val(currentParams.Keyword);
-}
-
-function createBrandItem(brand) {
-    var availabilityEnum = ['Online', 'In Store', 'All'];
-
-    var item = document.createElement('div');
-    item.className = 'brand';
-
-    var title = document.createElement('h2');
-    title.innerText = brand.brandName;
-
-    var retailerAvailability = document.createElement('p');
-    retailerAvailability.innerText = availabilityEnum[brand.retailerAvailability];
-
-    item.appendChild(title);
-    item.appendChild(retailerAvailability);
-
-    return item;
-}
-
-function createEmptyView() {
-    var empty = document.createElement('div');
-
-    empty.className = 'empty';
-    empty.innerText = 'There is no results for given search query';
-
-    return empty;
-}
-
-function setDefaultPageSize() {
-    modifySearchParams({ PageSize: PAGE_SIZE_OFFSET });
-}
-
-function setNextPageSize() {
-    var params = parseParams(location.search.substring(1));
-
-    modifySearchParams({ PageSize: parseInt(params.PageSize, 10) + PAGE_SIZE_OFFSET });
-}
-
-function searchBrands(options) {
-    var API_HOST = 'https://edf6f0c232.execute-api.ap-southeast-2.amazonaws.com/prod/';
-    var API_SUBJECT = 'Brands';
-
-    $('#promotions').hide();
-    $('#categories-grid').hide();
-    $('#featured').hide();
-    $('#load-more').hide();
-    $('#results').html('');
-    $('#search-results').show();
-    $('#loader').show();
-    if (options && options.loadMore) {
-        setNextPageSize();
-    } else {
-        setDefaultPageSize();
-    }
-
-    var currentParams = parseParams(location.search.substring(1));
-
-    $.ajax({
-        url: API_HOST + API_SUBJECT + '?' + location.search.substring(1)
-    }).done(function (e) {
-        $('#loader').hide();
-        $('#search-results-count').text(e.length);
-        if (e.length === 0) return $('#results').html(createEmptyView());
-        if (e.length === parseInt(currentParams.PageSize, 10)) $('#load-more').show();
-        $('#results').html(e.map(createBrandItem));
-    });
-}
-
-function getRetailerAvailability() {
-    var isOnline = $('#online').prop('checked');
-    var isInStore = $('#in-store').prop('checked');
-
-    if (isOnline && isInStore) return 'All';
-    if (isOnline) return 'Online';
-    if (isInStore) return 'InStore';
-
-    return '';
-}
-
-function handleChangeRetailerAvailability() {
-    modifySearchParams({ RetailerAvailability: getRetailerAvailability() });
-    searchBrands();
-}
-
 $('#category').change(function (e) {
-    modifySearchParams({ CategoryID: e.currentTarget.value });
-    searchBrands();
+    openpayUtils.modifySearchParams({CategoryID: e.currentTarget.value});
+    openpayUtils.searchBrands();
+});
+
+$('#city').change(function (e) {
+    openpayUtils.modifySearchParams({SuburbName: e.currentTarget.value});
+    openpayUtils.searchBrands();
 });
 
 $('#query').change(function (e) {
-    modifySearchParams({ Keyword: e.currentTarget.value });
-    searchBrands();
+    openpayUtils.modifySearchParams({Keyword: e.currentTarget.value});
+    openpayUtils.searchBrands();
 });
 
 $('#search-btn').click(function () {
-    searchBrands();
+    openpayUtils.searchBrands();
 });
 
-$('#categories-grid').click(function(e) {
+$('#categories-grid').click(function (e) {
     var CategoryID = $(e.target).data('value');
 
     if (CategoryID) {
-        modifySearchParams({ CategoryID: CategoryID });
-        searchBrands();
+        openpayUtils.modifySearchParams({CategoryID: CategoryID});
+        openpayUtils.searchBrands();
     }
 });
 
-$('#online').click(handleChangeRetailerAvailability);
-$('#in-store').click(handleChangeRetailerAvailability);
+$('#online').click(openpayUtils.handleChangeRetailerAvailability);
+$('#in-store').click(openpayUtils.handleChangeRetailerAvailability);
 $('#load-more').click(function () {
-    searchBrands({ loadMore: true })
+    openpayUtils.searchBrands({loadMore: true})
 });
 
-$(document).ready(function() {
-    populateControls();
-    var currentParams = parseParams(location.search.substring(1));
+$(document).ready(function () {
+    openpayUtils.populateControls();
+    var currentParams = openpayUtils.parseParams(location.search.substring(1));
 
     var shouldSearch = Object.keys(currentParams)
         .filter(function (key) {
@@ -151,7 +43,7 @@ $(document).ready(function() {
         .length > 0;
 
     if (shouldSearch) {
-        searchBrands();
+        openpayUtils.searchBrands();
     } else {
         $('#search-results').hide();
     }
