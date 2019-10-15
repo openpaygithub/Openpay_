3fcac55f5ff98a9836bb64d26538bd43455f3ace
MrNightingale <soloviov.v.v@gmail.com>
2019-10-15 23:14:27 +0300

    add base for the carousel on the shop page

--- src/scripts/shop.js
+++ src/scripts/shop.js
@@ -79,4 +79,22 @@ $(document).ready(function () {
             eventLabel: 'Esther',
         });
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        navContainer: '.retailers__carousel',
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    })
 });
